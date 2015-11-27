54dde4a2f758f54eda87260c93ab802a985cb2e8
zlobec <darjan.zlobec@gmail.com>
2015-11-27 16:22:56 +0100

    Create autoload.php

---
+++ autoload.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (c) 2005-2015 Leo Feyer
+ *
+ * @license LGPL-3.0+
+ */
+
+
+/**
+ * Register the namespaces
+ */
+ClassLoader::addNamespaces(array
+(
+	'Contao',
+));
+
+/**
+ * Register the templates
+ */
+TemplateLoader::addFiles(array
+(
+	'j_cookie.law'        => 'system/modules/j_eucookielaw/templates/jquery',
+));
